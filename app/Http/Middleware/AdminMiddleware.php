3f76cb1b73e7180afd571a6094923a331999971d
RubelPerez <rubel.daniel.perez@gmail.com>
2018-12-02 20:03:37 -0400

    Todo a la normalidad

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -15,11 +15,10 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (!auth()->user()->admin=="boss")
+        if (!auth()->user()->admin)
         {
             return redirect('/');
         }
 
         return $next($request);
     }
-}
